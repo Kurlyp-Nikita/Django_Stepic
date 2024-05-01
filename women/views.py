c7918a75c236b5bbcbdc00cc3724297bf046577d
Никита <ilinnikita244@gmail.com>
2024-05-01 16:37:26 +0300

    add db

---
+++ women/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.defaultfilters import slugify
+
+
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
+
+# class Myclass:
+#     def __init__(self, a, b):
+#         self.a = a
+#         self.b = b
+
+
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': '''<h1>Анджелина Джоли</h1> (англ. Angelina Jolie[7],
+    при рождении Войт (англ. Voight), ранее Джоли Питт (англ. Jolie Pitt); род. 4 июня 1975, Лос-Анджелес,
+    Калифорния, США) — американская актриса кино, телевидения и озвучивания, кинорежиссёр, сценаристка, продюсер,
+    фотомодель, посол доброй воли ООН.
+    Обладательница премии «Оскар», трёх премий «Золотой глобус»
+    (первая актриса в истории, три года подряд выигравшая премию) и двух «Премий Гильдии киноактёров США».''',
+     'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+
+]
+
+
+
+def index(request):
+    data = {'title': 'Главная страница',
+            'menu': menu,
+            # 'float': 28.56,
+            # 'lst': [1, 2, 'abc', True],
+            # 'set': {1, 2, 3, 4, 5},
+            # 'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+            # 'obj': Myclass(10, 20),
+            # 'url': slugify('The main page')
+            'posts': data_db,
+        }
+
+    return render(request, 'index.html', context=data)
+
+
+def about(request):
+    return render(request, 'about.html', {'title': 'О сайте', 'menu': menu})
+
+
+def show_post(request, post_id):
+    return HttpResponse(f'Отаброжение статьи с id = {post_id}')
+
+
+def addpage(request):
+    return HttpResponse('Добавление статьи')
+
+
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+
+def login(request):
+    return HttpResponse('Авторизация')
+
+
+def page_not_found(request, exeption):
+    return HttpResponse('<h1>Страница не найдена</h1>')
