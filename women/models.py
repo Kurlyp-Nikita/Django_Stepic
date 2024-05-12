6abd7289415e41c67d814119e147534f507e7ab5
Никита <ilinnikita244@gmail.com>
2024-05-12 21:53:58 +0300

    model manager, enumeration

--- women/models.py
+++ women/models.py
@@ -2,13 +2,25 @@ from django.db import models
 from django.urls import reverse
 
 
+class PublishedManager(models.Manager):  # Пользовательский менеджер модели
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)  # Виджеты для заполнения полей
+
+
 class Women(models.Model):
+    class Status(models.IntegerChoices):  # Создание перечисления
+        DRAFT = 0, 'Черновик'
+        PUBLISHED = 1, 'Опубликовано'
+
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)  # Виджеты для заполнения полей
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     def __str__(self):
         return self.title
